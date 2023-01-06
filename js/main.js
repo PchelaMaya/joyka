c61b60fe0f095ca2195bd226b398eaf6f4344c01
Maya <pchelaamaya@gmail.com>
2023-01-06 18:57:00 +0300

    add block send and correct block working

--- js/main.js
+++ js/main.js
@@ -1,76 +1,28 @@
-// // const select = document.querySelector('');
-// // Функция очистки класса
+//Carousel
 
-// function cleanSelect (select) {
+const owl = $('.owl-carousel');//находим элемент, на который будем вешать owlcarousel
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 20,
+  startPosition: 1,
+  items: 1,
+  responsive : {
+    1400 : {
+      items: 3,
+      margin: 20,
+    },
+}
+});//вызываем метод
 
-//     // Очищаем от стилей
+// Кнопка назад
 
-//     return $(select).removeClass('langen').removeClass('langru').removeClass('langsw')
+$('.slider__btn--left').click(function() {
+// При клике на кнопек будет вызываться trigger, карусель прокутит вперёд
+  owl.trigger('left.owl.carousel');
+})
 
-// }
-
- 
-
-// // Формируем select и его обработчики
-
-// function formSelect() {
-
-//     // получаем выпадающий список с уже очищенными классами
-
-//     var select = cleanSelect('.select-events');
-
- 
-
-//     // Добавляем класс, который соответствует выбранному элементу
-
-//     select.addClass(select.val() == '1' ? 'langen' : 'langru' : 'langsw' );
-
- 
-
-//     // Добавляем стили, чтобы у списка не было видно полосы прокрутки
-
-//     select.css({ height: 'auto', overflow: 'hidden', zIndex: '40000' });
-
- 
-
-//     // Определяем обработчик на событие ухода мышки с области элемента
-
-//     select.on('mouseleave', function () {
-
-//         // Устанавливаем обычный размер
-
-//         this.size = 1;
-
-//         // Добавляем класс стиля в соответствии с выбранным элементом
-
-//         cleanSelect(this).addClass($(this).val() == '1' ? 'langen' : 'langru' : 'langsw' );
-
-//     });
-
- 
-
-//     // Определяем обработчик на событие ухода мышки с области элемента
-
-//     select.on('mouseover', function () {
-
-//         // Очищаем стиль списка, чтобы он не мешал отображению
-
-//         cleanSelect(this);
-
-//         // Устанавливаем размер, равный количеству элементов
-
-//         this.size = $(this).find('option').length;
-
-//     });
-
-// }
-
- 
-
-// // После загрузки DOM - получаем список
-
-// $(function () {
-
-//     formSelect();
-
-// });
+// Кнопка вперёд
+$('.slider__btn--right').click(function() {
+    owl.trigger('right.owl.carousel');
+})
