d15c37c353ad0ce47424b1b373dc6991838da1d1
Maya <pchelaamaya@gmail.com>
2023-01-09 21:12:05 +0300

    add footer and correct slider

--- js/main.js
+++ js/main.js
@@ -1,31 +1,15 @@
-//Carousel
+    var swiper = new Swiper(".slider", {
+        // center: true,
+        loop: true,
+        initialSlide: 0,
+        slidesPerView: 5,
+      navigation: {
+        nextEl: ".slider__btn--left",
+        prevEl: ".slider__btn--right",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
 
-const owl = $('.owl-carousel');//находим элемент, на который будем вешать owlcarousel
-owl.owlCarousel({
-  center: true,
-  loop: true,
-  margin: 20,
-  startPosition: 1,
-  items: 1,
-  responsive : {
-    1400 : {
-      items: 3,
-      margin: 20,
-    },
-}
-});//вызываем метод
-
-// Кнопка назад
-
-$('.slider__btn--left').click(function() {
-// При клике на кнопек будет вызываться trigger, карусель прокутит вперёд
-  owl.trigger('buttons.left.owl.carousel');
-  console.log('left');
-})
-
-// Кнопка вперёд
-$('.slider__btn--right').click(function() {
-    owl.trigger('buttons.right.owl.carousel');
-    console.log('right');
-})
-// new Swiper('.owl-carousel');
+    });
